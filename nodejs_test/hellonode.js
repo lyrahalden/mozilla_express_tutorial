31ee93f2444861265a98d0b41d653f072cd6ee5b
Lyra Hall <lyrahall@gmail.com>
2018-01-08 17:15:05 -0800

    set up node.js dev environ w/eslinter for sublime text 3

---
+++ nodejs_test/hellonode.js
@@ -0,0 +1,18 @@
+//Example from Mozilla docs of how to make a node server without using Express
+//https://developer.mozilla.org/en-US/docs/Learn/Server-side/Express_Nodejs/development_environment
+
+//Load HTTP module
+var http = require("http");
+
+//Create HTTP server and listen on port 8000 for requests
+http.createServer(function (request, response) {
+
+   // Set the response HTTP header with HTTP status and Content type
+   response.writeHead(200, {'Content-Type': 'text/plain'});
+   
+   // Send the response body "Hello World"
+   response.end('Hello World\n');
+}).listen(8000);
+
+// Print URL for accessing server
+console.log('Server running at http://127.0.0.1:8000/')
